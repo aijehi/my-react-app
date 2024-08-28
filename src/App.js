152076e851176f70f4f74985b1a18bb5a6a20f74
aijehi <155052907+aijehi@users.noreply.github.com>
2024-08-28 10:37:09 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'; // This import is optional if you are using styles defined in App.css
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>Welcome to my simple React app.</p>
+      <p>First React app made from World of Aston, WOA</p>
+    </div>
+  );
+}
+
+export default App;
+
+
